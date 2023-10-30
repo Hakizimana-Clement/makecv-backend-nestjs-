bf4cdca051f59c3ac42582245d87560c9ae3d5e7
Hakizimana Clement <h.clementoert@gmail.com>
2023-10-30 23:57:27 +0200

    add dotenv

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config/dist';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          // url: 'mongodb://127.0.0.1:27017/curriculumVitaeDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1',
-
-          url: 'mongodb://127.0.0.1:27020/curriculumVitaeDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1',
+          // url: 'mongodb://127.0.0.1:27020/curriculumVitaeDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
